a27e62d3fdf60baf4b87bcdce4ec0edc26aa6ae5
stew <stewartanoya@gmail.com>
2025-09-08 22:47:39 -0400

    add third party contact table

---
+++ DDL/tables/third_party_contacts.sql
@@ -0,0 +1,15 @@
+CREATE TABLE third_party_contacts (
+  third_party_contact_id BIGSERIAL PRIMARY KEY,
+  agent_id   BIGINT NOT NULL
+             REFERENCES agents(user_id) ON DELETE RESTRICT,
+  name       TEXT,
+  email      TEXT,
+  phone      TEXT,
+  trade      TEXT,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMPTZ
+);
+
+CREATE INDEX ix_third_party_contacts_agent_id
+  ON third_party_contacts(agent_id);
